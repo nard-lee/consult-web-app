028f5cf7e6dee356754a52277821f1097aa88f42
__nardLee <59244491+nard-lee@users.noreply.github.com>
2024-03-09 12:12:47 +0800

    4nd commit

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ use App\Http\Controllers\UserController;
 
 Route::get('/', [MainController::class, 'Main'])->middleware('auth');
 
-Route::get('/form', [UserController::class, 'ViewForm']);
+Route::get('/form', [UserController::class, 'ViewForm'])->name('form');
 
 Route::get('/signup/{role}', [UserController::class, 'ViewSignup']);
 Route::post('/signup', [UserController::class, 'Signup'])->middleware('validate_new_user');
 
-Route::get('/login/{role}', [UserController::class, 'ViewLogin'])->name('login');
+Route::get('/login/{role}', [UserController::class, 'ViewLogin']);
 Route::post('/login', [UserController::class, 'Login'])->middleware('validate_user_data');
